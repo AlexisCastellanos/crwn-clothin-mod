9f971eff5d27fe3915ecffafa36ceb85c12e2e70
Alexis Castellanos <intelaki@gmail.com>
2020-03-15 21:34:00 -0600

    Updated project modules and added Send Email button in checkout page.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -38,6 +38,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div
+      className="email-button"
+      // onClick={() => clearItem(cartItem)}
+    >
+      Send Email
+    </div>
   </div>
 );
 
